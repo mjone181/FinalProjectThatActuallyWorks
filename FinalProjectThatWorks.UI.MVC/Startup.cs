045350d2fc2e073154cb2331bde69b9316320765
mjone181 <mjone181@gmail.com>
2021-01-25 14:33:06 -0600

    Added Initial UI Layer.

---
+++ FinalProjectThatWorks.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinalProjectThatWorks.UI.MVC.Startup))]
+namespace FinalProjectThatWorks.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
