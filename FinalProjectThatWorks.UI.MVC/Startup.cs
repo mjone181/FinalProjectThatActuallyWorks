1802754a300defd19c94217583df9f065e3e3893
mjone181 <mjone181@gmail.com>
2021-01-26 08:29:13 -0600

    Successfully created UI Layer.

--- FinalProjectThatWorks.UI.MVC/Startup.cs
+++ FinalProjectThatWorks.UI.MVC/Startup.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(FinalProjectThatWorks.UI.MVC.Startup))]
 namespace FinalProjectThatWorks.UI.MVC
 {
     public partial class Startup
