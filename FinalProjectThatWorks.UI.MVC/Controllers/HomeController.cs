8de878ce2a8e03aa08ce0d377a824486689abb2b
mjone181 <mjone181@gmail.com>
2021-01-30 21:39:21 -0600

    Successfully implemented and tested Contact Form

--- FinalProjectThatWorks.UI.MVC/Controllers/HomeController.cs
+++ FinalProjectThatWorks.UI.MVC/Controllers/HomeController.cs
@@ -1,4 +1,10 @@
 ﻿using System.Web.Mvc;
+//Needed for Email functionality.
+using System.Net.Mail;
+using System.Net;
+using System;
+//Required to make Contact View work.
+using FinalProjectThatWorks.UI.MVC.Models;
 
 namespace FinalProjectThatWorks.UI.MVC.Controllers
 {
@@ -25,6 +31,48 @@ namespace FinalProjectThatWorks.UI.MVC.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (ModelState.IsValid)
+            {
+                //If the Model is vaild send the email via a MailMessage.
+                string body = $"{cvm.Name} has sent you the following message: <br />" + $"{cvm.Message} <strong>from the email address</strong> {cvm.Email}";
+                MailMessage m = new MailMessage("no-reply@jonesmack.com", "mjone181@gmail.com", cvm.Subject, body);
+
+                //Allows HTML
+                m.IsBodyHtml = true;
+
+                //Makes Email high priority.
+                m.Priority = MailPriority.High;
+
+                //Reply to person who filled out form.
+                m.ReplyToList.Add(cvm.Email);
+
+                //Configure Mail client
+                SmtpClient client = new SmtpClient("mail.jonesmack.com");
+
+                //Configure Mail credentials
+                client.Credentials = new NetworkCredential("no-reply@jonesmack.com", "Allenwalker1!");
+
+                try
+                {
+                    //Send mail
+                    client.Send(m);
+                }
+                catch (Exception e)
+                {
+                    //Error message in ViewBag for Admins to see.
+                    ViewBag.Message = e.StackTrace;
+                    throw;
+                }
+
+                return View("EmailConfirmation");
+            }
+            return View(cvm);
+
         }
     }
 }
---
+++ FinalProjectThatWorks.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//Allows Email Form to work
+using System.ComponentModel.DataAnnotations;
+
+namespace FinalProjectThatWorks.UI.MVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "* Your name is required.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "* Your email is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+        //If used.
+        [Required(ErrorMessage = "* A subject is required.")]
+        public string Subject { get; set; }
+
+        [UIHint("MultilineText")]//Replaces textbox with textarea in the UI wherver an EditorFor() is located.
+        [Required(ErrorMessage = "* A message is required.")]
+        public string Message { get; set; }
+    }
+}
